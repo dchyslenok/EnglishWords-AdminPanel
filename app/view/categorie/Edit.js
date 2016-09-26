003ca5540cb789f1ee4bd8aea95b69dcd4e44106
dchyslenok <dchyslenok@gmail.com>
2016-09-26 22:20:05 +0300

    upload

--- app/view/categorie/Edit.js
+++ app/view/categorie/Edit.js
@@ -30,11 +30,6 @@ Ext.define('App.view.categorie.Edit', {
             name : 'imgUrl',
             allowBlank : false
         },{
-          xtype : 'button',
-          text : 'Select image',
-          iconCls : 'fa fa-floppy-o',
-          handler : 'onUploadFile'
-        },{
             xtype : 'checkbox',
             fieldLabel : 'Active',
             name : 'active',
@@ -46,7 +41,7 @@ Ext.define('App.view.categorie.Edit', {
         margin : 10,
         width: 202.33,
         height: 202.33,
-        html: '<img src="http://camaleon.tuzitio.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png" style="border: solid 5px #d0d0d0;" height="201.33" width="201.33">',
+        html: '<img src="http://englishwords/storage/image/FotorCreated.jpg" height="201.33" width="201.33">',
         listeners: {
             el: {
                 click: 'onUploadFile'
--- app/view/main/Uploader.js
+++ app/view/main/Uploader.js
@@ -1,99 +1,102 @@
 Ext.define('App.view.main.Uploader', {
   extend: 'Ext.Window',
-  alias: 'widget.nameCreationPopup',
-  title: 'aTitle',
+  title: 'Upload file',
   width: 450,
   height: 300,
-  layout: 'fit',
-  bodyStyle: 'padding:5px;',
   modal: true,
-  resizable: false,
+  layout: 'fit',
   constructor: function (callbeck) {
     var me = this;
     Ext.apply(this, {});
-
     me.items = [{
-      xtype : 'panel',
-      layout : 'form',
-      itemId : 'uploadForm',
-      items : [{
-        xtype : 'panel',
-        layout : 'hbox',
-        border : true,
-        items : [{
-          xtype : 'button',
-          text : 'Urk',
-          iconCls : 'fa fa-times',
-          handler : function () {
+      itemId: 'tab',
+      xtype: 'tabpanel',
+      plain: true,
+      items: [{
+        itemId: 'tab1',
+        xtype : 'form',
+        title: 'From URL',
+        layout: 'form',
+        anchor : 100,
+        items: [{
+          xtype : 'textfield',
+          vtype:'url',
+          itemId : 'ImgUrl',
+          fieldLabel : 'Image url',
+          name : 'imgUrl',
+          allowBlank : false
+        }],
+        buttons : [{
+          xtype: 'button',
+          text: 'Cancel',
+          iconCls: 'fa fa-times',
+          handler: function () {
             me.close();
           }
-        },{
-          xtype : 'button',
-          text : 'Upload file',
-          iconCls : 'fa fa-times',
-          handler : function () {
+        }, {
+          xtype: 'button',
+          text: 'Save',
+          formBind : true,
+          iconCls: 'fa fa-floppy-o',
+          handler: function () {
+            var url = me.getComponent('tab').getComponent('tab1').getComponent('ImgUrl').getValue();
+            callbeck(url);
             me.close();
           }
         }]
-      },{
-        reference: 'fileUpload',
-        xtype: 'filefield',
-        name: 'photo',
-        fieldLabel: 'Photo',
-        labelWidth: 50,
-        msgTarget: 'side',
-        allowBlank: false,
-        anchor: '100%',
-        buttonText: 'Select Photo...',
-        listeners: {
-          change: function () {
-            me.mask('Load');
-            var form = me.getComponent('uploadForm');
-            var file = form.down('filefield').getEl().down('input[type=file]').dom.files[0];
-            var type = file.name.split('.').pop();
-            var FR= new FileReader();
+      }, {
+        title: 'From File',
+        layout: 'form',
+        items: [{
+          xtype: 'filefield',
+          name: 'photo',
+          fieldLabel: 'Photo',
+          labelWidth: 50,
+          msgTarget: 'side',
+          allowBlank: false,
+          anchor: '100%',
+          buttonText: 'Select Photo...',
+          listeners: {
+            change: function (el) {
+              me.mask('Load');
+              var file = el.getEl().down('input[type=file]').dom.files[0];
+              var type = file.name.split('.').pop();
+              var FR = new FileReader();
 
-            FR.onload = function(e) {
-              Ext.Ajax.request({
-                url : 'http://englishwords/api_v1/upload/binary',
-                method : 'POST',
-                params : {
-                  data : e.target.result,
-                  type : type,
-                  name : file.name
-                },
-                success : function (response, opts) {
-                  var response = Ext.decode(response.responseText);
-                  callbeck(response.url);
-                  me.unmask();
-                  me.close();
-                },
-                failure : function (response, opts) {
-                  console.log('server-side failure with status code ' + response.status);
-                }
-              });
-            };
-            FR.readAsDataURL(file);
+              FR.onload = function (e) {
+                Ext.Ajax.request({
+                  url: 'http://englishwords/api_v1/upload/binary',
+                  method: 'POST',
+                  params: {
+                    data: e.target.result,
+                    type: type,
+                    name: file.name
+                  },
+                  success: function (response, opts) {
+                    var response = Ext.decode(response.responseText);
+                    callbeck(response.url);
+                    me.unmask();
+                    me.close();
+                  },
+                  failure: function (response, opts) {
+                    console.log('server-side failure with status code ' + response.status);
+                  }
+                });
+              };
+              FR.readAsDataURL(file);
+            }
           }
-        }
+        }],
+        buttons : [{
+          xtype: 'button',
+          text: 'Cancel',
+          iconCls: 'fa fa-times',
+          handler: function () {
+            me.close();
+          }
+        }]
       }]
     }];
-
-    me.buttons = [{
-      xtype : 'button',
-      text : 'Cancel',
-      iconCls : 'fa fa-times',
-      handler : function () {
-        me.close();
-      }
-    },{
-      xtype : 'button',
-      text : 'Save',
-      formBind : true,
-      iconCls : 'fa fa-floppy-o',
-      handler : callbeck
-    }];
-
     this.callParent(arguments);
   },
 });
